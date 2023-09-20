b65d055a136facb94e8789582c4689cae5fe280a
Anastasiya Blagodarnova <ablagodarnova@gmail.com>
2023-09-20 18:03:12 +0300

    docs: Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr = str.split('');
+  let openI;
+  let closeI;
+  let stack = [];
+  let open = [
+    '{',
+    '[',
+    '(',
+    ''
+  ];
+  let close = [
+    '}',
+    ')',
+    ']',
+    ''
+  ];
+for (let i = 0; i < arr.length; i++) {
+  openI = open.indexOf(arr[i]);
+  if (openI !== -1) {
+      stack.push(openI);
+}
+  closeI = close.indexOf(arr[i]);
+  if (closeI !== -1) {
+    openI = stack.pop()
+    if (openI !== closeI) {
+      return false
+    }
+  }
+}
+if(stack.length !== 0){
+  return false
+}
+return true
 }
